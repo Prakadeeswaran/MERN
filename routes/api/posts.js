22dae0808856836d2da56e51bbdd07bbf8638241
prakadees <prakadees@gmail.com>
2019-02-26 20:32:34 +0530

    Comments were added for routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// @route   GET /api/posts/test
+// @desc    posts test
+// @access  Public
 router.get("/test", (req, res) => {
   res.json({ msg: "Post test suucessfully" });
 });
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// @route   GET /api/users/test
+// @desc    user test
+// @access  Public
 router.get("/test", (req, res) => {
   res.json({ msg: "user test suucessfully" });
 });
