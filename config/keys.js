a1dd9e9909c3ad2ef30205e665381cde1ab8378d
prakadees <prakadees@gmail.com>
2019-02-26 08:36:50 +0530

    Initial commit with basic express setup and routes

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://prag:prag@cluster0-shard-00-00-se5l7.mongodb.net:27017,cluster0-shard-00-01-se5l7.mongodb.net:27017,cluster0-shard-00-02-se5l7.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+};
